64169e404a36a89066c01da17817a5658d3b9473
Helenwongyj <95870327+Helenwongyj@users.noreply.github.com>
2021-12-10 13:58:24 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-atrium_edited",
+      "name": "Atrium_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.1349292267465056,
+        "pitch": -0.026573798792425052,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19608138622230342,
+          "pitch": 0.14122717378346117,
+          "rotation": 0,
+          "target": "1-level-2_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1154479582400025,
+          "pitch": -0.08281120235792372,
+          "title": "School of Business",
+          "text": "Atrium Level 1"
+        }
+      ]
+    },
+    {
+      "id": "1-level-2_edited",
+      "name": "Level 2_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.004822935517207583,
+        "pitch": 0.06052556243106544,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.091867110922511,
+          "pitch": 0.10753470969310008,
+          "rotation": 0,
+          "target": "2-level-3_edited"
+        },
+        {
+          "yaw": 0.024889599010823105,
+          "pitch": 0.3719558602012878,
+          "rotation": 3.141592653589793,
+          "target": "0-atrium_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.03419391219859236,
+          "pitch": 0.014790250705054575,
+          "title": "School of Business",
+          "text": "Level 2"
+        }
+      ]
+    },
+    {
+      "id": "2-level-3_edited",
+      "name": "Level 3_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.845251392934264,
+        "pitch": -0.06435101937618448,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.808613261803343,
+          "pitch": 0.09423255614166592,
+          "rotation": 3.141592653589793,
+          "target": "1-level-2_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7977777405774749,
+          "pitch": -0.03999758795300146,
+          "title": "School of Business",
+          "text": "Level 3"
+        }
+      ]
+    }
+  ],
+  "name": "School Of Business",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
